f9804195cfb30acb77b01a92a3048d920df622ec
stureiko <stureiko@gmail.com>
2025-02-16 16:10:05 +0300

    added dvc pipeline

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 import seaborn as sns
-
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder, StandardScaler, RobustScaler, LabelEncoder, OneHotEncoder, MinMaxScaler
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_score, f1_score, roc_auc_score, recall_score
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LogisticRegression
@@ -75,6 +75,13 @@ def main():
 
     # Оценка качества
     print(classification_report(y_test, y_pred))
+
+    pd.DataFrame({'metrics': ['accuracy_score', 'precision_score', 'recall_score', 'f1_score', 'roc_auc_score'],
+              'values':[accuracy_score(y_test, y_pred), 
+              precision_score(y_test, y_pred),
+              recall_score(y_test, y_pred),
+              f1_score(y_test, y_pred),
+              roc_auc_score(y_test, y_pred)]}).to_csv('metrics.csv', index=False)
 
 if __name__ == "__main__":
     main()
